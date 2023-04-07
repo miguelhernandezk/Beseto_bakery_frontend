91b37712f3322a9f5c098e254edcf8af2bc0c292
Miguel Hernández <miguelhernandezk@gmail.com>
2023-04-06 18:35:40 -0600

    Fixed url in whatsapp messages

--- services/whatsapp.ts
+++ services/whatsapp.ts
@@ -2,7 +2,7 @@ const phoneNumber = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
 
 export const sendWhatsappMessage = (_id: string) => {
   window.open(
-    `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=Hola+me+podría+proporcionar+info+de+est+pastel%3F+https%3A%2F%2Fwww.beseto.com.mx%2Fproductos%2F${_id}&type=phone_number&app_absent=0`,
+    `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=Hola+me+podría+proporcionar+info+de+est+pastel%3F+https%3A%2F%2Fwww.beseto.com.mx%2Fproducts%2F${_id}&type=phone_number&app_absent=0`,
     '_blank'
   );
 };
