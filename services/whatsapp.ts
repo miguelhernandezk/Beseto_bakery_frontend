f8fddfa8ca30e70d96ee45aef5b411e0726461e8
Miguel Hernández <miguelhernandezk@gmail.com>
2023-04-06 18:30:54 -0600

    Push untracked whatsapp file

---
+++ services/whatsapp.ts
@@ -0,0 +1,8 @@
+const phoneNumber = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
+
+export const sendWhatsappMessage = (_id: string) => {
+  window.open(
+    `https://api.whatsapp.com/send/?phone=${phoneNumber}&text=Hola+me+podría+proporcionar+info+de+est+pastel%3F+https%3A%2F%2Fwww.beseto.com.mx%2Fproductos%2F${_id}&type=phone_number&app_absent=0`,
+    '_blank'
+  );
+};
