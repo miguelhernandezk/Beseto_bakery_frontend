f4053b33266dbed5ba99aaab56c8955c440575c7
Miguel Hernández <miguelhernandezk@gmail.com>
2023-05-28 13:19:50 -0600

    Force newest products to appear first

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -21,7 +21,7 @@ export default function Products() {
       return undefined;
     } else {
       setLoading(false);
-      setProducts(productsResponse.data);
+      setProducts(productsResponse.data.reverse());
     }
   };
   useEffect(() => {
