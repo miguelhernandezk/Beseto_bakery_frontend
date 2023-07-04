2e88dfc8589db4497a456b1c533b982471f36d2d
miguelhernandezk <65262291+miguelhernandezk@users.noreply.github.com>
2023-07-03 19:35:19 -0600

    Added orders feature

--- pages/carrito.tsx
+++ pages/carrito.tsx
@@ -13,6 +13,7 @@ import {
   Button,
   Stack,
   Typography,
+  CircularProgress,
 } from '@mui/material';
 import { useSession } from 'next-auth/react';
 import CloseIcon from '@mui/icons-material/Close';
@@ -26,12 +27,17 @@ import { CartItem, CartItemDto } from '../interfaces/User';
 import { updateCart } from '../services/users';
 import { ToastContainer } from 'react-toastify';
 import Link from 'next/link';
+import { GetServerSideProps } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]';
+import { Role } from '../interfaces/Enums';
 
 function Carrito() {
   const { cart, setCart, notifyError, notifySuccess } = useContext(AppContext);
   const { data: session, status } = useSession();
   const [amounts, setAmounts] = useState<(number | string)[]>([]);
   const [tempAmount, setTempAmount] = useState<number>();
+  const [uiState, setUiState] = useState<'loading' | 'info loaded'>();
 
   const handleChangeAmount = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
@@ -122,6 +128,9 @@ function Carrito() {
 
   useEffect(() => {
     setAmounts(cart.map((cartItem) => cartItem.amount));
+    if (cart !== undefined) {
+      setUiState('info loaded');
+    }
   }, [cart]);
 
   return (
@@ -135,92 +144,111 @@ function Carrito() {
         />
         <HeroHeader />
         <Container className="pt-16">
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead className="bg-beseto-bisque">
-                <TableRow>
-                  <TableCell />
-                  <TableCell />
-                  <TableCell align="left">Producto</TableCell>
-                  <TableCell align="left">Precio</TableCell>
-                  <TableCell align="left">Cantidad</TableCell>
-                  <TableCell align="left">Subtotal</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {cart.map((cartItem, index) => (
-                  <TableRow
-                    key={cartItem.product.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      <IconButton
-                        onClick={() =>
-                          handleDeleteProduct(cartItem.product._id)
-                        }
+          {cart.length > 0 && uiState === 'info loaded' && (
+            <>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead className="bg-beseto-bisque">
+                    <TableRow>
+                      <TableCell />
+                      <TableCell />
+                      <TableCell align="left">Producto</TableCell>
+                      <TableCell align="left">Precio</TableCell>
+                      <TableCell align="left">Cantidad</TableCell>
+                      <TableCell align="left">Subtotal</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {cart.map((cartItem, index) => (
+                      <TableRow
+                        key={cartItem.product.name}
+                        sx={{
+                          '&:last-child td, &:last-child th': { border: 0 },
+                        }}
                       >
-                        <CloseIcon />
-                      </IconButton>
-                    </TableCell>
-                    <TableCell align="right">
-                      <img
-                        src={cartItem.product.picture[0]}
-                        className="w-8"
-                        alt={cartItem.product.name}
-                      />
-                    </TableCell>
-                    <TableCell align="left">{cartItem.product.name}</TableCell>
-                    <TableCell align="left">
-                      {new Intl.NumberFormat('es-MX', {
-                        style: 'currency',
-                        currency: 'MXN',
-                      }).format(cartItem.product.price)}
-                    </TableCell>
-                    <TableCell align="left">
-                      <TextField
-                        type="number"
-                        value={amounts[index]}
-                        onChange={(event) => handleChangeAmount(event, index)}
-                        onBlur={() => handleLeftAmountField(index)}
-                      />
-                    </TableCell>
-                    <TableCell align="left">
-                      {new Intl.NumberFormat('es-MX', {
-                        style: 'currency',
-                        currency: 'MXN',
-                      }).format(cartItem.amount * cartItem.product.price)}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Stack alignItems="flex-end">
-            <Typography variant="h5" className="mt-10">
-              Total:{' '}
-              {new Intl.NumberFormat('es-MX', {
-                style: 'currency',
-                currency: 'MXN',
-              }).format(
-                cart.reduce(
-                  (accumulator, currentItem) =>
-                    accumulator +
-                    currentItem.amount * currentItem.product.price,
-                  0
-                )
-              )}
-            </Typography>
-            <Box>
-              <Link href="/checkout">
-                <Button
-                  variant="contained"
-                  className="bg-beseto-dark-gray text-white grow mt-10 flex flex-col justify-center items-center"
-                >
-                  Realizar pedido
-                </Button>
-              </Link>
-            </Box>
-          </Stack>
+                        <TableCell component="th" scope="row">
+                          <IconButton
+                            onClick={() =>
+                              handleDeleteProduct(cartItem.product._id)
+                            }
+                          >
+                            <CloseIcon />
+                          </IconButton>
+                        </TableCell>
+                        <TableCell align="right">
+                          <img
+                            src={cartItem.product.picture[0]}
+                            className="w-8"
+                            alt={cartItem.product.name}
+                          />
+                        </TableCell>
+                        <TableCell align="left">
+                          {cartItem.product.name}
+                        </TableCell>
+                        <TableCell align="left">
+                          {new Intl.NumberFormat('es-MX', {
+                            style: 'currency',
+                            currency: 'MXN',
+                          }).format(cartItem.product.price)}
+                        </TableCell>
+                        <TableCell align="left">
+                          <TextField
+                            type="number"
+                            value={amounts[index]}
+                            onChange={(event) =>
+                              handleChangeAmount(event, index)
+                            }
+                            onBlur={() => handleLeftAmountField(index)}
+                          />
+                        </TableCell>
+                        <TableCell align="left">
+                          {new Intl.NumberFormat('es-MX', {
+                            style: 'currency',
+                            currency: 'MXN',
+                          }).format(cartItem.amount * cartItem.product.price)}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <Stack alignItems="flex-end">
+                <Typography variant="h5" className="mt-10">
+                  Total:{' '}
+                  {new Intl.NumberFormat('es-MX', {
+                    style: 'currency',
+                    currency: 'MXN',
+                  }).format(
+                    cart.reduce(
+                      (accumulator, currentItem) =>
+                        accumulator +
+                        currentItem.amount * currentItem.product.price,
+                      0
+                    )
+                  )}
+                </Typography>
+                <Box>
+                  <Link href="/checkout">
+                    <Button
+                      variant="contained"
+                      className="bg-beseto-dark-gray text-white grow mt-10 flex flex-col justify-center items-center"
+                    >
+                      Realizar pedido
+                    </Button>
+                  </Link>
+                </Box>
+              </Stack>
+            </>
+          )}
+          {cart.length <= 0 && uiState === 'info loaded' && (
+            <Typography variant="h3">Tu carrito está vacío</Typography>
+          )}
+          {uiState === 'loading' && (
+            <>
+              <CircularProgress />
+              <Typography variant="h3">Cargando</Typography>
+            </>
+          )}
         </Container>
         <Box className="mt-8">
           <Footer />
@@ -229,5 +257,29 @@ function Carrito() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<object> = async (
+  context
+) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (session && session.user.role === Role.CUSTOMER) {
+    return {
+      notFound: true,
+    };
+  }
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+};
 
 export default Carrito;
